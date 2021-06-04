78b342120b40581a5d2846cf672d09d828fd7856
Indrit Breti <om3ga0verride@gmail.com>
2021-06-04 09:12:33 +0200

    Added jump using mouse to avoid keyboard event handling

--- main.py
+++ main.py
@@ -2,24 +2,18 @@ import turtle
 import random
 import time
 from PIL import Image
+import keyboard
 
 # global variables
 score = 0
 timeLimit = 999999
 startTime = time.time()
 
-# screen
-screen = turtle.Screen()
-screen.tracer(0)  # update delay 0
-width=1000
-height=600
-screen.setup(width, height)
-
-# add shapes that will be used
-screen.addshape("fox.gif")
-screen.addshape("coin.gif")
-screen.addshape("background.gif")
-
+img = Image.open("background.gif")
+# print(img.size)
+width=img.size[0]+200
+height=img.size[1]+10
+print("w: ", width, " h: ", height, img.size)
 
 class Label(turtle.Turtle):
     def __init__(self, text="Default Text", x=0, y=0, textcolor="black", align="center",
@@ -41,6 +35,68 @@ class Label(turtle.Turtle):
         self.clear()
         self.write(text, align=self.align, font=self.font)
 
+class Heart(turtle.Turtle):
+    def __init__(self, size, fill, x, y):
+        super().__init__(visible=1)
+        self.penup()
+        self.size=size
+        self.fill=fill
+        self.x=x
+        self.y=y
+        self.goto(self.x, self.y)
+        self.pensize(1)
+        self.color('red', 'red')
+        # if fill:
+        #     self.color('red', 'red')
+        # else:
+        #     self.fillcolor('red')
+        self.begin_fill()
+
+        self.left(140)
+        self.forward(111.65*size)
+
+        self.func()
+        self.left(120)
+        self.func()
+
+        self.forward(111.65*size)
+
+        #self.forward(100)
+        self.hideturtle()
+        self.end_fill()
+
+    def func(self):
+        for i in range(200):
+            self.right(1)
+            self.forward(1*self.size)
+
+class Lives(turtle.Turtle):
+    def __init__(self, num_lives, x, y):
+        super().__init__(shape='square', visible=0)
+        self.size = 0.15
+        self.space=40
+        # print(x)
+        self.x=x-num_lives*self.space
+        self.y=y-self.size*240
+        # print(x)
+        self.numLives=num_lives
+        self.currentNumLives=self.numLives
+        self.hearts=[]
+        for i in range(self.numLives):
+            h=Heart(self.size, 0, self.x+self.space*i, self.y+0)
+            self.hearts.append(h)
+
+    def add(self):
+        index=self.numLives-self.currentNumLives-1
+        if index>-1 and index<self.numLives:
+            self.hearts[index]=Heart(self.size, 0, self.x+self.space*index, self.y+0)
+            self.currentNumLives+=1
+    def remove(self):
+        index=self.numLives-self.currentNumLives
+        if index > -1 and index < self.numLives:
+            self.hearts[index].clear()
+            self.currentNumLives-=1
+
 
 class Player(turtle.Turtle):
     def __init__(self):
@@ -49,6 +105,7 @@ class Player(turtle.Turtle):
         self.penup()
         self.speed(0)
         self.setheading(90)
+        self.enableJump()
 
     def moveRight(self):
         (x, y) = self.pos()
@@ -67,6 +124,25 @@ class Player(turtle.Turtle):
     def moveDown(self):
         if self.ycor() > -400:
             self.backward(25)
+
+    def moveDown(self):
+        self.backward(40)
+
+    def jump(self, x, y):
+        print("------------")
+        jump_duration=0.2
+        start_time=time.time()
+        self.forward(40)
+        screen.update()
+        screen.ontimer(self.moveDown, 200)
+        # while 1:
+        #     if time.time()>=start_time+jump_duration:
+        #         self.backward(30)
+        #         print('======')
+        #         break
+    def enableJump(self):
+        screen.onclick(self.jump)
+        # screen.onkeypress(self.jump, "space")
 
     def enableMovement(self):
         screen.onkeypress(self.moveUp, "w")
@@ -104,45 +180,61 @@ class Wall(turtle.Turtle):
     def __init__(self):
         super().__init__(shape='square', visible=0)
         self.img = Image.open("background.gif")
-        print(self.img.size)
+        # print(self.img.size)
+        self.position=0
+        self.bgtracker=0
+        self.step=30
+        self.bgArray=[]
 
+        for i in range(4):
+            self.bg0=turtle.Turtle(shape='square', visible=1)
+            self.bg0.shape("background.gif")
+            self.bg0.penup()
+            self.bgArray.append(self.bg0)
 
-        self.bg0=turtle.Turtle(shape='square', visible=1)
-        self.bg0.shape("background.gif")
-        self.bg0.penup()
-        self.bg0.goto(0, 0)
+        self.leftMost=0
+        self.rightMost=3
 
-        self.bg1=turtle.Turtle(shape='square', visible=1)
-        self.bg1.shape("background.gif")
-        self.bg1.penup()
-        self.bg1.goto(self.img.size[0], 0)
-
-        self.bg2 = turtle.Turtle(shape='square', visible=1)
-        self.bg2.shape("background.gif")
-        self.bg2.color('red')
-        self.bg2.penup()
-
-        self.bg2.goto(-self.img.size[0], 0)
+        self.bgArray[0].goto(-self.img.size[0], 0)
+        self.bgArray[1].goto(0, 0)
+        self.bgArray[2].goto(self.img.size[0], 0)
+        self.bgArray[3].goto(self.img.size[0]*2, 0)
 
         self.enableMovement()
 
 
 
     def moveLeft(self):
-        print(self.bg2.pos())
-        self.bg0.forward(30)
-        self.bg1.forward(30)
-        self.bg2.forward(30)
+        if self.bgtracker<-self.img.size[0]+200: # add 200 as a spacer on the left to avoid white bg from transition
+            self.bgArray[self.rightMost].goto(self.bgArray[self.leftMost].pos()[0]-self.img.size[0], 0)
+
+            self.leftMost=self.rightMost
+            self.rightMost-=1
+            if self.rightMost<0:
+                self.rightMost=3
+            self.bgtracker+=self.img.size[0]
+
+        if self.position>-30:
+            for bg in self.bgArray:
+                bg.forward(self.step)
+            self.position-=self.step
+            self.bgtracker-=self.step
 
     def moveRight(self):
-        print(self.bg2.pos())
-        print(self.bg2.pos()[0]+self.img.size[0])
-        if self.bg2.pos()[0]+self.img.size[0]<=0:
-            pass
-            #self.bg2.goto(self.bg1.pos()[0]+self.img.size[0], 0)
-        self.bg0.backward(25)
-        self.bg1.backward(25)
-        self.bg2.backward(25)
+        self.position += self.step
+        self.bgtracker += self.step
+
+        if self.bgtracker>self.img.size[0]:
+            self.bgArray[self.leftMost].goto(self.bgArray[self.rightMost].pos()[0]+self.img.size[0], 0)
+            self.bgtracker-=self.img.size[0]
+            self.rightMost=self.leftMost
+            self.leftMost+=1
+            if self.leftMost>3:
+                self.leftMost=0
+
+        for bg in self.bgArray:
+            bg.backward(self.step)
+
 
     def enableMovement(self):
         screen.onkeypress(self.moveLeft, "a")
@@ -232,6 +324,17 @@ def GameOver():
 
 
 if __name__ == "__main__":
+    # screen
+    screen = turtle.Screen()
+    screen.tracer(0, 0)  # update delay 0
+    screen.setup(width, height)
+
+
+    # add shapes that will be used
+    screen.addshape("fox.gif")
+    screen.addshape("coin.gif")
+    screen.addshape("background.gif")
+
     screen.listen()
 
     bg = Wall()
@@ -240,12 +343,12 @@ if __name__ == "__main__":
     player0 = Player()
     player0.goto(-400, -100)
     #q1 = Question()
-
+    lives = Lives(5, width/2, height/2)
     while True:
         screen.update()
-
         #q1.amimate()
-
+        screen.onkeypress(lives.remove, 'f')
+        screen.onkeypress(lives.add, 'g')
         # countdown
         timeElapsed = int(time.time() - startTime)
         timer.setText("Timer: {}".format(timeLimit - timeElapsed))
